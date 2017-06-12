1f79a6d6a09dd1390c71a19d0b7b90b1968959bc
mandyjor <mandy1jor@gmail.com>
2017-06-12 11:37:48 -0500

    Fixed unary operator when it is not in a binary operation

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,22 +12,32 @@ import Darwin
 class ViewController: UIViewController {
     
     @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var sequenceDisplay: UILabel!
     
     var isTyping = false
-    var isDecimal = false
+    var alreadyDecimal = false
     
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
         
         if isTyping {
-            if !isDecimal {
-                let textCurrentlyInDisplay = display.text!
-                display.text = textCurrentlyInDisplay + digit
-            }
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
         }else {
             display.text = digit
             isTyping = true
-            
+            if let resultIsPending = brain.getResultIsPending{
+                if !resultIsPending {
+                    brain.performOperation("clear")
+                }
+            }
+        }
+    }
+    
+    @IBAction func touchDecimal(_ sender: UIButton) {
+        if !alreadyDecimal {
+            touchDigit(sender)
+            alreadyDecimal = true
         }
     }
     
@@ -40,20 +50,38 @@ class ViewController: UIViewController {
         }
     }
     
-    fileprivate var brain = CalculatorBrain()
+    private var brain = CalculatorBrain()
     
     @IBAction func performOperation(_ sender: UIButton) {
         if isTyping{
             brain.setOperand(displayValue)
             isTyping = false
+            alreadyDecimal = false
         }
         
         if let mathematicalSymbol = sender.currentTitle {
             brain.performOperation(mathematicalSymbol)
         }
+        
         if let result = brain.result {
             displayValue = result
         }
+        
+        if let sequence = brain.sequenceDisplay{
+            if let resultIsPending = brain.getResultIsPending{
+                if resultIsPending {
+                    sequenceDisplay.text = sequence + " ..."
+                }else{
+                    sequenceDisplay.text = sequence + " ="
+                }
+            }
+        }
+    }
+    
+    @IBAction func clear(_ sender: UIButton) {
+        displayValue = 0
+        sequenceDisplay.text = "--"
+        brain.performOperation("clear")
     }
     
 }
--- CalculatorBrain.swift
+++ CalculatorBrain.swift
@@ -7,3 +7,143 @@
 //
 
 import Foundation
+
+struct CalculatorBrain {
+    
+    private var accumulator: Double?
+    var result: Double? {
+        get{
+            return accumulator
+        }
+    }
+    
+    private var currentSequence: String?
+    var sequenceDisplay: String? {
+        get{
+            return currentSequence
+        }
+    }
+    
+    private var resultIsPending = false
+    var getResultIsPending: Bool? {
+        get{
+            return resultIsPending
+        }
+    }
+    
+    private var nested = false
+    
+    private enum Operation {
+        case constant(Double)
+        case unary((Double) -> Double)
+        case binary((Double, Double) -> Double)
+        case equals
+        case clear
+    }
+    
+    private var operations: Dictionary<String, Operation> = [
+        "π": Operation.constant(Double.pi),
+        "e": Operation.constant(M_E),
+        "√": Operation.unary(sqrt),
+        "pow": Operation.binary({pow($0, $1)}),
+        "cos": Operation.unary(cos),
+        "sin": Operation.unary(sin),
+        "tan": Operation.unary(tan),
+        "±": Operation.unary({ -$0 }),
+        "x": Operation.binary({$0 * $1}),
+        "÷": Operation.binary({$0 / $1}),
+        "+": Operation.binary({$0 + $1}),
+        "-": Operation.binary({$0 - $1}),
+        "=": Operation.equals,
+        "clear": Operation.clear,
+    ]
+    
+    private var pendingBinaryOperation: PendingBinaryOperation?
+    
+    mutating func performOperation(_ symbol: String) {
+        if let operation = operations[symbol]{
+            switch operation{
+            case .constant(let value):
+                accumulator = value
+                
+                if currentSequence != nil {
+                    if resultIsPending {
+                        currentSequence = currentSequence! + " " + symbol
+                        nested=true
+                    }else{
+                        currentSequence = symbol
+                    }
+                }else{
+                    currentSequence = symbol
+                }
+            case .unary(let function):
+                if accumulator != nil {
+                    if currentSequence != nil {
+                        if resultIsPending {
+                            currentSequence = currentSequence! + " " + symbol + "(" + String(accumulator!) + ")"
+                            nested=true
+                        }else{
+                            currentSequence = symbol + "(" + currentSequence! + ")"
+
+                        }
+                    }else{
+                        currentSequence = symbol + "(" + String(accumulator!) + ")"
+                    }
+                    
+                    accumulator = function(accumulator!)
+                    print(currentSequence!)
+                }
+            case .binary(let function):
+                if accumulator != nil {
+                    pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!)
+                    if currentSequence != nil {
+                        currentSequence = currentSequence! + " " + symbol
+
+                    }else{
+                        currentSequence = String(accumulator!) + " " + symbol
+                    }
+                    
+                    accumulator = nil
+                    resultIsPending = true
+                    print("Result is Pending: \(resultIsPending)")
+                }
+            case .equals:
+                performPendingBinaryOperation()
+            case .clear:
+                accumulator = nil
+                currentSequence = nil
+            }
+            
+        }
+    }
+    
+    private struct PendingBinaryOperation{
+        let function: (Double, Double) -> Double
+        let firstOperand: Double
+        
+        func perform(with secondOperand: Double) -> Double {
+            return function(firstOperand, secondOperand)
+        }
+    }
+    
+    mutating private func performPendingBinaryOperation() {
+        if accumulator != nil && pendingBinaryOperation != nil && currentSequence != nil {
+            if !nested{
+                currentSequence = currentSequence! + " " + String(accumulator!)
+            }
+            nested = false
+
+            accumulator = pendingBinaryOperation!.perform(with:accumulator!)
+            print(currentSequence!)
+
+            
+            pendingBinaryOperation = nil
+            resultIsPending = false
+            print("Result is Pending: \(resultIsPending)")
+        }
+    }
+    
+    mutating func setOperand(_ operand: Double) {
+        accumulator = operand
+    }
+}
